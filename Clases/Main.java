04d40974e559a5ec956a1497f2c3e56d6af25a0b
nitek10 <60189365+nitek10@users.noreply.github.com>
2020-05-24 03:10:04 +0200

    .

--- Clases/Main.java
+++ Clases/Main.java
@@ -1,9 +1,15 @@
 package Clases;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.print.DocFlavor.URL;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
@@ -15,6 +21,17 @@ public class Main {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					BufferedImage bImage = null;
+					try {
+						File initialImage = new File("C:\\Users\\Jorge\\eclipse-workspace\\PROYECTO 3EVALUACION\\src\\Imagenes\\btnFinalizar.png");
+			             bImage = ImageIO.read(initialImage);
+			             ImageIO.write(bImage, "jpg", new File("C:\\Users\\Jorge\\eclipse-workspace\\PROYECTO 3EVALUACION\\src\\lauritacallateya.jpg"));
+					} catch (IOException e) {
+						   System.out.println("Error de escritura");
+						   e.printStackTrace();
+					}
+					
+					
 					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 					VentanaPrincipal frame = new VentanaPrincipal();
 				} catch (Exception e) {
